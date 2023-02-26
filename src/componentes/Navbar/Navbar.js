fdd978faac57842d269693003e3c4161be12b728
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2023-02-26 17:16:29 -0300

    acomode el css

--- src/componentes/Navbar/Navbar.js
+++ src/componentes/Navbar/Navbar.js
@@ -14,34 +14,38 @@ const Navbar = () => {
             <div className="container-fluid">
               
               <div className='cajaLogo'>
-
-              <a className="navbar-brand logo" href=""><img src='../img/logo.png' style={{width: "150px"}}/></a>
-
-                     <div className='cajaTexto'>
-                       <h1 className='texto'>Atenea Mallas</h1>
-                     </div>
-
-                    <button className="navbar-toggler botonBurger" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                   <span className="navbar-toggler-icon"></span>.
-
-                   <div>
-                       <NavLink to="/Carrito">{carrito.length}<img src="https://e7.pngegg.com/pngimages/603/776/png-clipart-shopping-cart-shopping-cart.png" alt="" style={{width: "40px"}} />{carrito.length}</NavLink>
-                    </div>
+                <div className='logo'>
+                     <a className="navbar-brand" href=""><img className='logoAtenea' src='../img/logo.png' /></a>
+                </div>
+                 
+                 <button className="navbar-toggler botonBurger" type="button" data-bs-toggle="collapse"             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">  
+                        <span className="navbar-toggler-icon"></span>.
                  </button>
+
               </div>
+
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    
+
                     <form className="d-flex">
 
-                      <button type='submit' style={{width: "80px", border: "2rem", background: "pink"}}> <NavLink to="/Login" style={{color: "#373737"}}>Login</NavLink></button>
+                      <div className='cajaBotonLogin'>
+                        
+                          <button className='botonLogin' type='submit'><NavLink to="/Login" style={{color: "#373737"}}>Login</NavLink></button>
 
-                       <div>
-                             <NavLink to="/Carrito">{carrito.length}<img className='imgCarrito' src="https://e7.pngegg.com/pngimages/603/776/png-clipart-shopping-cart-shopping-cart.png" alt="" style={{width: "40px"}} /></NavLink>
-                       </div>
+                          <div className='cajaBotonCarrito'>
+
+                              <button className='botonCarrito'><NavLink to="/Carrito">{carrito.length}<img src="https://e7.pngegg.com/pngimages/603/776/png-clipart-shopping-cart-shopping-cart.png" alt="" style={{width: "65px"}} /></NavLink></button>
+                             
+                          </div>
+
+                      </div>
+
                     </form>
-    
-                 </div>
+
+              </div>
+
+              
             </div>
       </nav>
     </div>
