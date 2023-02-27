7891a51333b0ee3cd473b2b9afdb49128907f464
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2023-02-27 14:47:41 -0300

    sin terminar

--- src/componentes/carrito/Carrito.jsx
+++ src/componentes/carrito/Carrito.jsx
@@ -2,13 +2,17 @@ import React, { useContext } from 'react'
 import { CarritoContext } from '../../context/CarritoContext'
 import CarritoItem from '../CarritoItem/CarritoItem';
 
+
 const Carrito = () => {
 
   const {carrito}  = useContext (CarritoContext);
 
 
   return carrito.length === 0 ? (
-    <div>No hay productos aún</div>
+    <div>No hay productos aún
+
+    </div>
+
   ) : (
     <div>
         {carrito.map((item) => (
